b29cd1a5a1243bd3bf2f9be023a7705d3d6e1398
xuenjun <xuenjun@12308.com>
2016-11-22 13:53:42 +0800

    add a node server

--- share/learn/nodejs/demo6.js
+++ share/learn/nodejs/demo6.js
@@ -1,7 +1,13 @@
-console.log("asdfasdf debug atom");
-console.log("asdfasdf debug atom");
-console.log("asdfasdf debug atom");
-console.log("asdfasdf debug atom");
-console.log("asdfasdf debug atom");
-console.log("asdfasdf debug atom");
-console.log("asdfasdf debug atom");
+var fs = require('fs');
+var http = require('http');
+
+http.createServer(function(req,res){
+  console.log("req");
+  res.writeHeader(200);
+  res.write('ok');
+  var aa = fs.readFileSync('api.js');
+  res.write(aa);
+  res.end();
+}).listen(9999).on('error',function(err){
+  console.log(err);
+})
